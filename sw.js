835e662a35a47968ad601d53b6cf8fc699177d71
Germán Cabezas <153135388+germocab@users.noreply.github.com>
2026-06-03 12:29:41 -0400

    3.0
    Push Notifications included

--- sw.js
+++ sw.js
@@ -41,3 +41,30 @@ self.addEventListener('fetch', e => {
       .catch(() => caches.match(e.request))
   );
 });
+
+
+
+
+
+
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || '/icons/apple-touch-icon.png',
+      badge: '/icons/favicon-32x32.png',
+      vibrate: [200, 100, 200]
+    };
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/') // Abre la webapp al hacer clic
+  );
+});
